736db1ac078c3321844bebebbcb6301e8246f34f
joeban <joeanddaneil5@gmail.com>
2024-10-21 17:45:41 +0800

    t

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import { getUserAsync } from '$lib/api/getUser';
+	import type { UserInfo } from '$lib/type';
 	import { onMount } from 'svelte';
 
-	onMount(() => {
-		getUserAsync();
-	});
+	let userList: UserInfo[] = [];
+	/* 
+			init()
+				- 拿到 siteData 分別給 siteEntries and sitePromises
+				- siteEntries 
+					一開始 initial 畫面 loading
+				- 透過 sitePromises 拿回每個 site 狀態
+
+			$: if (inited) {}
+			- 初始化完畢，拿到 sitePromises and initial siteEntries loading..
+			- forEach 每一個 sitePromises 並拿出每個 promise 結果，塞回 siteEntries
+  */
+	const init = () => {
+
+  };
 </script>
 
 <h1>Welcome to SvelteKit</h1>
